d20b6729b2745a52983916d242bb0193456d21ba
ALEX4NDR303 <calexandre.gonc@gmail.com>
2025-08-20 01:42:05 +0000

    ex5.c

--- ex4.c
+++ ex4.c
@@ -3,17 +3,18 @@
 
 int main(){
 
-    printf("Calculadora de hipotenusa\n");
+    printf("Calculadora de hipotenusa");
 
     float cat1,cat2;
 
     printf("Digite o primeiro cateto");
     scanf("%f",&cat1);
-    printf("Digite o segundoo cateto");
+    printf("Digite o segundo cateto");
     scanf("%f",&cat2);
 
     float h = sqrt(cat1*cat1 + cat2*cat2);
-    scanf("%f",&h);
+    
     printf("Um triângulo retângulo com lados %.2f e %.2f tem uma hipotenusa igual a %.2f",cat1,cat2,h);
 
+    return 0;
 }
---
+++ ex5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <tgmath.h>
+
+int main() {
+
+    printf("Calculadora de área de círculo");
+    float raio, area;
+
+    printf("Digite o raio do círculo: ");
+    scanf("%f", &raio);
+
+    area = (raio * raio) * ;
+
+    printf("Um círculo com raio %.2f tem área igual a %.4f", raio, area);
+
+    return 0;
+    
+}
